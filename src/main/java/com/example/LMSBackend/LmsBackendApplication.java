55a4315a61fcd5a6cf55ab42efc36397b4791a91
SajeeRodrigo <sajee.thamanga@gmail.com>
2022-10-09 00:05:51 +0530

    just sent an email to reciver

--- src/main/java/com/example/LMSBackend/LmsBackendApplication.java
+++ src/main/java/com/example/LMSBackend/LmsBackendApplication.java
@@ -1,12 +1,18 @@
 package com.example.LMSBackend;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class LmsBackendApplication {
+	@Autowired
+	private EmailSenderService senderService;
 
 	public static void main(String[] args) {
 
@@ -18,5 +24,11 @@ public class LmsBackendApplication {
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendEmail(){
+		senderService.sendEmail("sajee.thamanga@gmail.com","This is subject","This is body");
+	}
+
 
 }
