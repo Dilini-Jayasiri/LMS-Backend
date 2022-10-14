de3eb95527e7348ed0bf9ca679cdc0ce5939d54d
kumesh1997 <tharindukumesh09@gmail.com>
2022-10-14 17:58:27 +0530

    Cross Origin issues solved by Kumesh

--- src/main/java/com/example/LMSBackend/Controller/MarksController.java
+++ src/main/java/com/example/LMSBackend/Controller/MarksController.java
@@ -15,7 +15,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/marks")
-@CrossOrigin
+@CrossOrigin(origins ="http://localhost:3006")
 public class MarksController {
     @Autowired
     private final MarksService marksservice;
