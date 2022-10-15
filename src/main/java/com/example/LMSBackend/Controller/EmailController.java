8951d59b49f55dd546e440349a4b2849a4b2241b
SajeeRodrigo <sajee.thamanga@gmail.com>
2022-10-15 17:35:22 +0530

    cross origin added

--- src/main/java/com/example/LMSBackend/Controller/EmailController.java
+++ src/main/java/com/example/LMSBackend/Controller/EmailController.java
@@ -2,11 +2,14 @@ package com.example.LMSBackend.Controller;
 
 import com.example.LMSBackend.Model.EmailMessage;
 import com.example.LMSBackend.Service.EmailSenderService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+//@CrossOrigin
 @RequestMapping("/teacher")
 public class EmailController {
 
