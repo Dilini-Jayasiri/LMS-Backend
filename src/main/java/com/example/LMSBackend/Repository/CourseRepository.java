5f8fe9c25479c10cf01323a9756c478fdc59cc5c
tharindu767 <74450668+tharindu767@users.noreply.github.com>
2022-10-16 15:07:16 +0530

    Update CourseRepository.java

--- src/main/java/com/example/LMSBackend/Repository/CourseRepository.java
+++ src/main/java/com/example/LMSBackend/Repository/CourseRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CourseRepository extends JpaRepository<Course, String>{
-
+//select data from course table when given course id
     @Query(value = "SELECT * FROM course WHERE course_id =?;",nativeQuery = true)
     Course getCourseByCourseID(String course_id);
 
